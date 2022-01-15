108bb9cb2f4b3f97adca5fa8cb09e070ba1b4afc
NgocTM13 <ngoctm1099@gmail.com>
2022-01-15 22:17:45 +0700

    fix import react-bootstrap to reactstrap

--- src/conponents/profile-user/HeaderProfile.tsx
+++ src/conponents/profile-user/HeaderProfile.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Button } from 'react-bootstrap'
+import { Button } from 'reactstrap'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 import { followUser, unFollowUser } from '../../api/user.api'
